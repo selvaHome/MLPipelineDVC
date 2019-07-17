be70a6a9d054a95c9ff563cac2862fa2d3bf9f20
selvaHome <ulag.selva@gmail.com>
2019-07-17 17:43:41 +0200

    new branch training Gradient Boosting

--- MLPipelineDVC/src/trainModel.py
+++ MLPipelineDVC/src/trainModel.py
@@ -8,8 +8,8 @@ from sklearn.ensemble import GradientBoostingRegressor
 dataTrain = np.loadtxt(open('/Users/selva/Documents/linkedinPosts/rae2822Airfoil/MLPipelineDVC/data/data_processed/dataTrain.csv',"rb"), delimiter=",")
 
 # build model
-model = LinearRegression().fit(dataTrain[:,:-1], dataTrain[:,-1])
-#model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=1, random_state=0, loss='ls').fit(dataTrain[:,:-1], dataTrain[:,-1])
+#model = LinearRegression().fit(dataTrain[:,:-1], dataTrain[:,-1])
+model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=1, random_state=0, loss='ls').fit(dataTrain[:,:-1], dataTrain[:,-1])
 
 # save model artifact
 os.makedirs("/Users/selva/Documents/linkedinPosts/rae2822Airfoil/MLPipelineDVC/outputs", exist_ok = True)
