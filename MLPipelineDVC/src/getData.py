758e69af890e6f39782f61dc5babed963bd5b978
selvaHome <ulag.selva@gmail.com>
2019-07-17 16:52:20 +0200

    DVC is initialized

--- MLPipelineDVC/src/getData.py
+++ MLPipelineDVC/src/getData.py
@@ -1,11 +1,5 @@
-import os
 import numpy as np
-import urllib.request
 
-# get data from original source
-urllib.request.urlretrieve(url = "http://airfoiltools.com/polar/csv?polar=xf-rae2822-il-50000", 
-                           filename = "./data/data.csv")
-
-# skip the initial info. in the data file
-data_actual = np.loadtxt(open("./data/data.csv", "rb"), delimiter=",", skiprows=11)
-np.savetxt("./data/data.csv", data_actual, delimiter=",")
+# skip the initial info. in the raw data file
+data_actual = np.loadtxt(open("/Users/selva/Documents/linkedinPosts/rae2822Airfoil/MLPipelineDVC/data/data_raw.csv", "rb"), delimiter=",", skiprows=11)
+np.savetxt("/Users/selva/Documents/linkedinPosts/rae2822Airfoil/MLPipelineDVC/data/data_actual/data_actual.csv", data_actual, delimiter=",")
--- MLPipelineDVC/src/processData.py
+++ MLPipelineDVC/src/processData.py
@@ -1,9 +1,9 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-data = np.loadtxt(open('./data/data.csv', "rb"), delimiter=",",skiprows=0)
+data = np.loadtxt(open('/Users/selva/Documents/linkedinPosts/rae2822Airfoil/MLPipelineDVC/data/data_actual/data_actual.csv', "rb"), delimiter=",",skiprows=0)
 xTrain, xTest, yTrain, yTest = train_test_split(np.hstack((data[:,[0]],data[:,[5]], data[:,[6]])), data[:,[1]], test_size = 1/3, random_state = 0)
 
 
-np.savetxt("./data/dataTrain.csv", np.column_stack((xTrain,yTrain)), delimiter=",")
-np.savetxt("./data/dataTest.csv", np.column_stack((xTest,yTest)), delimiter=",")
+np.savetxt("/Users/selva/Documents/linkedinPosts/rae2822Airfoil/MLPipelineDVC/data/data_processed/dataTrain.csv", np.column_stack((xTrain,yTrain)), delimiter=",")
+np.savetxt("/Users/selva/Documents/linkedinPosts/rae2822Airfoil/MLPipelineDVC/data/data_processed/dataTest.csv", np.column_stack((xTest,yTest)), delimiter=",")
